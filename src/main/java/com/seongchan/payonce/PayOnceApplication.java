362dc7254785919349530be94afdb929fa1c54f7
Seongchan-CH0I <chltjdcks329@naver.com>
2026-09-20 15:47:34 +0900

    초기 세팅

---
+++ src/main/java/com/seongchan/payonce/PayOnceApplication.java
@@ -0,0 +1,13 @@
+package com.seongchan.payonce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayOnceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayOnceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seongchan/payonce/PayOnceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seongchan.payonce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayOnceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
